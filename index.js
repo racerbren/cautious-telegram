3e74a420b334b93555d5d123b76110b42b3d5761
aseng2 <98761137+aseng2@users.noreply.github.com>
2023-02-25 17:21:39 -0800

    Add files via upload

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-console.log('Hello World');
+// const condition = [];
+// condition.push("Sunny", "Cloudy", "Windy", "Rainy", "Stormy");
+// var temps = 100; 
+// var conditions = condition;
+// var city = "Long Beach";
+const condition = document.querySelector(".condition");
+const icon = document.querySelector(".icon");
+const temps = document.querySelector(".temp"); 
+
+
+window.addEventListener("click", () =>{
+    const APIkey = "b5baf800d5aaab557d6896a90df24d76";
+    const city = document.querySelector(".search-box input").value;
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${APIkey}`)
+        .then((response) => {response.json();})
+        .then((data) => {console.log(data);
+            temps.textContent = data.main.temp;
+            condition.textContent = data.weather.description;
+            icon = data.weather.icon;
+        });
+});
+
+
+
